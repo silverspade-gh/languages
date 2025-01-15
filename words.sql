f4a314df671a0337589d0c3ba2bbf7ebbea59747
silverspade-gh <silver@bucketofdata.com>
2025-01-15 16:05:10 -0500

    Update words.sql
    Added column

--- words.sql
+++ words.sql
@@ -24,19 +24,21 @@ VALUES (
 ALTER TABLE words
 ADD COLUMN hanzi_story VARCHAR(500),
 ADD COLUMN hanzi_mnemonic VARCHAR(500),
-ADD COLUMN hanzi_radicals VARCHAR(300);
+ADD COLUMN hanzi_radicals VARCHAR(300),
+ADD COLUMN word_type VARCHAR(20);
 
 -- Modification of a row
 UPDATE words
 SET 
-  bengali =  'আমি',
-  bengali_latin = 'Ami',
-  english = 'I',
-  spanish = 'yo',
-  hanzi_story = 'The umbrella acts as a halberd that slashes monsters across the fence',
-  hanzi_mnemonic = 'The (wo3)rrior carries the mighty umbrella.',
-  hanzi_radicals = 'umbrella, halberd, slash'
-WHERE hanzi = '我';
+  bengali =  'পারতে',
+  bengali_latin = 'parte',
+  english = 'can',
+  spanish = 'puedo',
+  hanzi_story = 'The man meets at two inside of a private room.',
+  hanzi_mnemonic = 'A man who can run such errands is able to (hui3)t.',
+  hanzi_radicals = 'man, two, private',
+  word_type = 'modal verb'
+WHERE hanzi = '会';
 
 -- Adding a new row (which could also be done from a .csv, API or other manner)
 INSERT INTO words (hanzi, pinyin, bengali, bengali_latin, english, spanish, hanzi_story, hanzi_mnemonic, hanzi_radicals)
@@ -45,5 +47,5 @@ VALUES
     '们', 'men5', 'রা', 'ra', '-s', '-s',
     'You apply the magical powers of the staff onto the gate to widen it for more than one.',
     'Now multiple (men5) and women can pass through the gate!',
-    'staff, gate'
+    'staff, gate' -- Note that there is a null entry: word_type was not specified because plurals don't classify as types.
 )
